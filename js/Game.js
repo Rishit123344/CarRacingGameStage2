8510a25e94bbeab13d9bb7e08678a2bb649f15ac
Rishit123344 <bramarasunku@gmail.com>
2020-02-02 12:58:54 +0530

    structured code

--- js/Game.js
+++ js/Game.js
@@ -15,12 +15,39 @@ class Game {
     });
   }
 
-  start(){
+  async start(){
     if(gameState === 0){
       player = new Player();
-      player.getCount();
+      var playercountref = await database.ref('playerCount').once("value");
+      if(playercountref.exists()){
+        playerCount = playercountref.val();
+        player.getCount();
+      }
+      
       form = new Form()
       form.display();
+    }
+  }
+  play (){
+    form.hide();
+    textSize(30)
+    text("Game Start",120,100)
+    Player.getPlayerinfo();
+    if(allPlayers!==undefined){
+      var displayPosition = 130;
+      for(var plr in allPlayers){
+        if(plr === "player"+player.index)
+        fill ("red");
+        else
+        fill ("black");
+        displayPosition+=20;
+        textSize(15);
+        text(allPlayers[plr].name+":"+allPlayers[plr].distance,120,displayPosition);
+      }
+    }
+    if(keyIsDown(UP_ARROW)&&player.index!==null){
+      player.distance+=50;
+      player.update();
     }
   }
 }
